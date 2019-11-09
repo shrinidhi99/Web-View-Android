493b08535188966ea772c7b016f4e34287d0015c
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-11-09 15:50:39 +0530

    web view

---
+++ README.md
@@ -0,0 +1,1 @@
+Web View in Android
---
+++ app/src/main/java/com/example/web_view_android/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.web_view_android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        iv = findViewById(R.id.iv);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.mytransition);
+        iv.startAnimation(myanim);
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
